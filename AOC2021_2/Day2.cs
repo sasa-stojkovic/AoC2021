fe89bc63e91ed70485f3ab1242f5d60c9aa25c30
sasa <sasa@fulcrumpro.com>
2021-12-02 13:33:35 +0100

    day 2

--- AOC2021_2/Day2.cs
+++ AOC2021_2/Day2.cs
@@ -4,6 +4,66 @@ namespace AOC2021_2
 {
     public class Day2
     {
+        public static int MultiplyYourFinalHorizontalPositionByYourFinalDepth(string Input)
+        {
+            Input = Input?.ToLower();
+            var ResultH = 0;
+            var ResultV = 0;
 
+            var AllLines = Input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var Line in AllLines)
+            {
+                var LineCommands = Line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var Value = Convert.ToInt32(LineCommands[1]);
+
+                switch (LineCommands[0])
+                {
+                    case "up":
+                        ResultV -= Value;
+                        break;
+                    case "down":
+                        ResultV += Value;
+                        break;
+                    case "forward":
+                        ResultH += Value;
+                        break;
+                }
+            }
+            var Result = ResultH * ResultV;
+            return Result;
+        }
+
+        public static int MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim(string Input)
+        {
+            Input = Input?.ToLower();
+            var ResultH = 0;
+            var ResultV = 0;
+            var Aim = 0;
+
+            var AllLines = Input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var Line in AllLines)
+            {
+                var LineCommands = Line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var Value = Convert.ToInt32(LineCommands[1]);
+
+                switch (LineCommands[0])
+                {
+                    case "up":
+                        //ResultV -= Value;
+                        Aim -= Value;
+                        break;
+                    case "down":
+                        //ResultV += Value;
+                        Aim += Value;
+                        break;
+                    case "forward":
+                        ResultH += Value;
+                        ResultV += Value * Aim;
+                        break;
+                }
+            }
+            var Result = ResultH * ResultV;
+            return Result;
+        }
     }
 }
--- AOC2021_2_Test/Day2Test.cs
+++ AOC2021_2_Test/Day2Test.cs
@@ -7,9 +7,22 @@ namespace AOC2021_2_Test
     public class Day2Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MultiplyYourFinalHorizontalPositionByYourFinalDepth()
         {
-            
+            var res1 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepth(AOC2021_1_Test.Day1Inputs.Input1);
+            Assert.IsTrue(res1 == 150);
+
+            var res2 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepth(AOC2021_1_Test.Day1Inputs.Input2);
+        }
+
+        
+        [TestMethod]
+        public void MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim()
+        {
+            var res1 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim(AOC2021_1_Test.Day1Inputs.Input1);
+            Assert.IsTrue(res1 == 900);
+
+            var res2 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim(AOC2021_1_Test.Day1Inputs.Input2);
         }
     }
 }
