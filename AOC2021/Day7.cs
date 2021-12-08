19d8a071090df952a5681fe5114a74245b92964b
sasa <sasa@fulcrumpro.com>
2021-12-08 12:59:09 +0100

    Day 7

--- AOC2021/Day7.cs
+++ AOC2021/Day7.cs
@@ -4,11 +4,63 @@ using System.Linq;
 
 namespace AOC2021
 {
+    public class Possition
+    {
+        public int Value { get; set; }
+        public int Count { get; set; }
+    }
+
     public class Day7
     {
-        public static int Method1(string Input) 
+        public static long HowMuchFuelMustSpendToAlign(string Input) 
         {
-            return 1; 
+            long Price = Int32.MaxValue;
+            long Allign = -1;
+
+            var Positions = Input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(i => Convert.ToInt32(i)).GroupBy(i => i).Select(i => new Possition() { Value = i.Key, Count = i.Count() }).ToList();
+
+            for (long j = 0; j < Positions.Max(i => i.Value); j++)
+            {
+                var TmpPrice = Positions.Select(i => Math.Abs(i.Value - j) * i.Count).Sum();
+                if (TmpPrice < Price) 
+                {
+                    Price = TmpPrice;
+                    Allign = j;
+                }
+            }
+
+            return Price;
+        }
+
+        public static long HowMuchFuelMustSpendToAlignExp(string Input)
+        {
+            long Price = Int32.MaxValue;
+            long Allign = -1;
+
+            var Positions = Input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(i => Convert.ToInt32(i)).GroupBy(i => i).Select(i => new Possition() { Value = i.Key, Count = i.Count() }).ToList();
+
+            for (long j = 0; j < Positions.Max(i => i.Value); j++)
+            {
+                var TmpPrice = Positions.Select(i => MovePrice(j > i.Value ? i.Value : j, j > i.Value ? j : i.Value) * i.Count).Sum();
+                if (TmpPrice < Price)
+                {
+                    Price = TmpPrice;
+                    Allign = j;
+                }
+            }
+
+            return Price;
+        }
+
+        private static long MovePrice(long Pos, long Target)
+        {
+            long Sum = 0;
+            int Step = 1;
+            for (long i = Pos; i < Target; i++)
+            {
+                Sum += Step++;
+            }
+            return Sum;
         }
     }
 }
--- AOC2021_Test/Day7Test.cs
+++ AOC2021_Test/Day7Test.cs
@@ -8,12 +8,21 @@ namespace AOC2021_Test
     public class Day7Test
     {
         [TestMethod]
-        public void Method1()
+        public void HowMuchFuelMustSpendToAlign()
         {
-            var res1 = Day7.Method1(Day7Inputs.Input1);
-            Assert.IsTrue(res1 == 1);
+            var res1 = Day7.HowMuchFuelMustSpendToAlign(Day7Inputs.Input1);
+            Assert.IsTrue(res1 == 37);
 
-            var res2 = Day7.Method1(Day7Inputs.Input2);
+            var res2 = Day7.HowMuchFuelMustSpendToAlign(Day7Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void HowMuchFuelMustSpendToAlignExp()
+        {
+            var res1 = Day7.HowMuchFuelMustSpendToAlignExp(Day7Inputs.Input1);
+            Assert.IsTrue(res1 == 168);
+
+            var res2 = Day7.HowMuchFuelMustSpendToAlignExp(Day7Inputs.Input2);
         }
     }
 }
