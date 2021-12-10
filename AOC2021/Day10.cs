4828ac2507ccb4dbd2a44473773aa649aba94de8
sasa <sasa@fulcrumpro.com>
2021-12-10 10:21:09 +0100

    Day 10

---
+++ AOC2021/Day10.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2021
+{
+    public class Day10
+    {
+        public static int TotalSyntaxErrorScore(string Input)
+        {
+            int Result = 0;
+
+            var LinesS = Input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> Points = new Dictionary<string, int>() {
+                        { ")", 3 },
+                        { "]", 57 },
+                        { "}", 1197 },
+                        { ">", 25137}
+                    };
+
+            foreach (var LineS in LinesS)
+            {
+                Stack<string> Stack = new Stack<string>();
+                foreach (var C in LineS)
+                {
+                    var IsValid = true;
+                    var Character = C.ToString();
+                    switch (Character)
+                    {
+                        case "(":
+                        case "[":
+                        case "{":
+                        case "<":
+                            Stack.Push(Character);
+                            break;
+                        case ")":
+                            var OpeneChar1 = Stack.Pop();
+                            if (OpeneChar1 != "(")
+                            {
+                                IsValid = false;
+                                Result += Points[")"];
+                            }
+                            break;
+                        case "]":
+                            var OpeneChar2 = Stack.Pop();
+                            if (OpeneChar2 != "[")
+                            {
+                                IsValid = false;
+                                Result += Points["]"];
+                            }
+                            break;
+                        case "}":
+                            var OpeneChar3 = Stack.Pop();
+                            if (OpeneChar3 != "{")
+                            {
+                                IsValid = false;
+                                Result += Points["}"];
+                            }
+                            break;
+                        case ">":
+                            var OpeneChar4 = Stack.Pop();
+                            if (OpeneChar4 != "<")
+                            {
+                                IsValid = false;
+                                Result += Points[">"];
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                    if (!IsValid)// && Stack.Count == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return Result;
+        }
+
+        public static long MiddleScore(string Input)
+        {
+            long Result = 0;
+
+            var LinesS = Input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<long> LineScores = new List<long>();
+
+            Dictionary<string, int> Points = new Dictionary<string, int>() {
+                        { ")", 1 },
+                        { "]", 2 },
+                        { "}", 3 },
+                        { ">", 4}
+                    };
+
+            foreach (var LineS in LinesS)
+            {
+                Stack<string> Stack = new Stack<string>();
+                long LineScore = 0;
+                var IsValid = true;
+
+                foreach (var C in LineS)
+                {
+                    var Character = C.ToString();
+                    switch (Character)
+                    {
+                        case "(":
+                        case "[":
+                        case "{":
+                        case "<":
+                            Stack.Push(Character);
+                            break;
+                        case ")":
+                            var OpeneChar1 = Stack.Pop();
+                            if (OpeneChar1 != "(") IsValid = false;
+                            break;
+                        case "]":
+                            var OpeneChar2 = Stack.Pop();
+                            if (OpeneChar2 != "[") IsValid = false;
+                            break;
+                        case "}":
+                            var OpeneChar3 = Stack.Pop();
+                            if (OpeneChar3 != "{") IsValid = false;
+                            break;
+                        case ">":
+                            var OpeneChar4 = Stack.Pop();
+                            if (OpeneChar4 != "<") IsValid = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (IsValid && Stack.Count > 0)
+                {
+                    while (Stack.Count > 0)
+                    {
+                        var OpeneChar = Stack.Pop();
+                        switch (OpeneChar)
+                        {
+                            case "(":
+                                LineScore = (LineScore * 5) + Points[")"];
+                                break;
+                            case "[":
+                                LineScore = (LineScore * 5) + Points["]"];
+                                break;
+                            case "{":
+                                LineScore = (LineScore * 5) + Points["}"];
+                                break;
+                            case "<":
+                                LineScore = (LineScore * 5) + Points[">"];
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    LineScores.Add(LineScore);
+                }
+            }
+
+            int Index = LineScores.Count / 2;
+            var Ordered = LineScores.OrderBy(i => i).ToList();
+            Result = Ordered[Index];
+
+            return Result;
+        }
+    }
+}
---
+++ AOC2021_Test/DayTest.cs
@@ -0,0 +1,228 @@
+using AOC2021;
+using AOC2021_Test.Inputs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AOC2021_Test
+{
+    [TestClass]
+    public class Day1Test
+    {
+        [TestMethod]
+        public void HowManyMeasurementsAreLargerThanThePreviousMeasurement()
+        {
+            var count1 = Day1.HowManyMeasurementsAreLargerThanThePreviousMeasurement(Day1Inputs.Input1);
+            Assert.IsTrue(count1 == 7);
+
+            var count2 = Day1.HowManyMeasurementsAreLargerThanThePreviousMeasurement(Day1Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void HowManyMeasurementsAreLargerThanThePreviousMeasurement3SlotWindow()
+        {
+            var count1 = Day1.HowManyMeasurementsAreLargerThanThePreviousMeasurement3SlotWindow(Day1Inputs.Input1);
+            Assert.IsTrue(count1 == 5);
+
+            var count2 = Day1.HowManyMeasurementsAreLargerThanThePreviousMeasurement3SlotWindow(Day1Inputs.Input2);
+        }
+    }
+
+    [TestClass]
+    public class Day2Test
+    {
+        [TestMethod]
+        public void MultiplyYourFinalHorizontalPositionByYourFinalDepth()
+        {
+            var res1 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepth(Day2Inputs.Input1);
+            Assert.IsTrue(res1 == 150);
+
+            var res2 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepth(Day2Inputs.Input2);
+        }
+
+
+        [TestMethod]
+        public void MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim()
+        {
+            var res1 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim(Day2Inputs.Input1);
+            Assert.IsTrue(res1 == 900);
+
+            var res2 = Day2.MultiplyYourFinalHorizontalPositionByYourFinalDepthWithAim(Day2Inputs.Input2);
+        }
+    }
+
+    [TestClass]
+    public class Day3Test
+    {
+        [TestMethod]
+        public void PowerConsumptionOfTheSubmarine()
+        {
+            var ret1 = Day3.PowerConsumptionOfTheSubmarine(Day3Inputs.Input1);
+            Assert.IsTrue(ret1 == 198);
+
+            var ret2 = Day3.PowerConsumptionOfTheSubmarine(Day3Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void LifeSupportRating()
+        {
+            var ret1 = Day3.LifeSupportRating(Day3Inputs.Input1);
+            Assert.IsTrue(ret1 == 230);
+
+            var ret2 = Day3.LifeSupportRating(Day3Inputs.Input2);
+        }
+    }
+
+    [TestClass]
+    public class Day4Test
+    {
+        [TestMethod]
+        public void FinalScore()
+        {
+            var res1 = Day4.FinalScore(Day4Inputs.Input1A, Day4Inputs.Input1B);
+            Assert.IsTrue(res1 == 4512);
+
+            var res2 = Day4.FinalScore(Day4Inputs.Input2A, Day4Inputs.Input2B);
+        }
+
+        [TestMethod]
+        public void FinalScoreLastToWin()
+        {
+            var res1 = Day4.FinalScoreLastToWin(Day4Inputs.Input1A, Day4Inputs.Input1B);
+            Assert.IsTrue(res1 == 1924);
+
+            var res2 = Day4.FinalScoreLastToWin(Day4Inputs.Input2A, Day4Inputs.Input2B);
+        }
+    }
+
+    [TestClass]
+    public class Day5Test
+    {
+        [TestMethod]
+        public void HowManyPointsOverlapHV()
+        {
+            var res1 = Day5.HowManyPointsOverlap(Day5Inputs.Input1, false);
+            Assert.IsTrue(res1 == 5);
+
+            var res2 = Day5.HowManyPointsOverlap(Day5Inputs.Input2, false);
+        }
+
+        [TestMethod]
+        public void HowManyPointsOverlapD()
+        {
+            var res1 = Day5.HowManyPointsOverlap(Day5Inputs.Input1, true);
+            Assert.IsTrue(res1 == 12);
+
+            var res2 = Day5.HowManyPointsOverlap(Day5Inputs.Input2, true);
+        }
+    }
+
+    [TestClass]
+    public class Day6Test
+    {
+        [TestMethod]
+        public void HowManyLanternfishWouldThereBe()
+        {
+            var res1 = Day6.HowManyLanternfishWouldThereBe("1", 5);
+            Assert.IsTrue(res1 == 2);
+
+            res1 = Day6.HowManyLanternfishWouldThereBe(Day6Inputs.Input1, 18);
+            Assert.IsTrue(res1 == 26);
+
+            res1 = Day6.HowManyLanternfishWouldThereBe(Day6Inputs.Input1, 80);
+            Assert.IsTrue(res1 == 5934);
+
+            res1 = Day6.HowManyLanternfishWouldThereBe(Day6Inputs.Input1, 256);
+            Assert.IsTrue(res1 == 26984457539);
+
+            var res2 = Day6.HowManyLanternfishWouldThereBe(Day6Inputs.Input2, 256);
+        }
+    }
+
+    [TestClass]
+    public class Day7Test
+    {
+        [TestMethod]
+        public void HowMuchFuelMustSpendToAlign()
+        {
+            var res1 = Day7.HowMuchFuelMustSpendToAlign(Day7Inputs.Input1);
+            Assert.IsTrue(res1 == 37);
+
+            var res2 = Day7.HowMuchFuelMustSpendToAlign(Day7Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void HowMuchFuelMustSpendToAlignExp()
+        {
+            var res1 = Day7.HowMuchFuelMustSpendToAlignExp(Day7Inputs.Input1);
+            Assert.IsTrue(res1 == 168);
+
+            var res2 = Day7.HowMuchFuelMustSpendToAlignExp(Day7Inputs.Input2);
+        }
+    }
+
+    [TestClass]
+    public class Day8Test
+    {
+        [TestMethod]
+        public void HowManyTimesDigits1478Appear()
+        {
+            var res1 = Day8.HowManyTimesDigits1478Appear(Day8Inputs.Input1);
+            Assert.IsTrue(res1 == 26);
+
+            var res2 = Day8.HowManyTimesDigits1478Appear(Day8Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void AddUpAllOutputValues()
+        {
+            var res1 = Day8.AddUpAllOutputValues(Day8Inputs.Input1);
+            Assert.IsTrue(res1 == 61229);
+
+            var res2 = Day8.AddUpAllOutputValues(Day8Inputs.Input2);
+        }
+    }
+
+    [TestClass]
+    public class Day9Test
+    {
+        [TestMethod]
+        public void SumOfRiskLevelsOfLowPoints()
+        {
+            var res1 = Day9.SumOfRiskLevelsOfLowPoints(Day9Inputs.Input1);
+            Assert.IsTrue(res1 == 15);
+
+            var res2 = Day9.SumOfRiskLevelsOfLowPoints(Day9Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void MultiplyTogetherSizesOfThreeLargestBasins()
+        {
+            var res1 = Day9.MultiplyTogetherSizesOfThreeLargestBasins(Day9Inputs.Input1);
+            Assert.IsTrue(res1 == 1134);
+
+            var res2 = Day9.MultiplyTogetherSizesOfThreeLargestBasins(Day9Inputs.Input2);
+        }
+    }
+
+    [TestClass]
+    public class Day10Test
+    {
+        [TestMethod]
+        public void TotalSyntaxErrorScore()
+        {
+            var res1 = Day10.TotalSyntaxErrorScore(Day10Inputs.Input1);
+            Assert.IsTrue(res1 == 26397);
+
+            var res2 = Day10.TotalSyntaxErrorScore(Day10Inputs.Input2);
+        }
+
+        [TestMethod]
+        public void MiddleScore()
+        {
+            var res1 = Day10.MiddleScore(Day10Inputs.Input1);
+            Assert.IsTrue(res1 == 288957);
+
+            var res2 = Day10.MiddleScore(Day10Inputs.Input2);
+        }
+    }
+}
